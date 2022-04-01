8634ddaf29253b5ef4f0fb521c9a2c7e554afc43
Anshil Gandhi <gandhi21299@gmail.com>
2022-04-01 14:48:44 -0600

    Completed: - added debug and release modes - fixed edge squares rotation errors - Implemented L,Li,R,Ri,U,Ui
    TODO:
    - implement D,Di
    - create a test suite

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 
 CC = clang++
 CFLAGS = -std=c++17 -O3
+SRC = rubik3.cc
 
-rubik3: rubik3.cc
-	$(CC) $(CFLAGS) $< -o $@
+debug: $(SRC)
+	mkdir -p build-debug
+	$(CC) $(CFLAGS) $< -o build-debug/rubik3 -DDEBUG
+
+release: $(SRC)
+	mkdir -p build-release
+	$(CC) $(CFLAGS) $< -o build-release/rubik3
 
 clean:
-	rm -rf rubik3
+	rm -rf build-debug build-release
--- rubik3.cc
+++ rubik3.cc
@@ -42,6 +42,24 @@ std::ostream& operator<<(std::ostream& out, const Face& face){
     return out;
 }
 
+std::ostream& operator<<(std::ostream& out, const Move& move){
+    switch(move){
+    case R:     out << "R";     break;
+    case Ri:    out << "Ri";    break;
+    case L:     out << "L";     break;
+    case Li:    out << "Li";    break;
+    case B:     out << "B";     break;
+    case Bi:    out << "Bi";    break;
+    case D:     out << "D";     break;
+    case Di:    out << "Di";    break;
+    case F:     out << "F";     break;
+    case Fi:    out << "Fi";    break;
+    case U:     out << "U";     break;
+    case Ui:    out << "Ui";    break;
+    }
+    return out;
+}
+
 struct Rubik3{
     std::map<Face, std::vector<Color>> cube;
 
@@ -50,6 +68,7 @@ struct Rubik3{
     void show_face(Face faceId);
     void unfold_and_show();
     Rubik3 next(Move move);
+    bool is_identity();
 };
 
 Rubik3::Rubik3(){
@@ -82,7 +101,6 @@ void Rubik3::show(){
 }
 
 void Rubik3::unfold_and_show(){
-    
     auto print_space = [](int n){
         for (int i = 0; i < n; ++i) std::cout << ' ';
     };
@@ -123,30 +141,10 @@ void Rubik3::unfold_and_show(){
 }
 
 Rubik3 Rubik3::next(Move move){
+    #ifdef DEBUG
+    std::cout << "Action: " << move << '\n';
+    #endif
     Rubik3 new_rubik = *this;
-    
-    // Define a lambda for face rotation. 
-    // Remark, other side of the edge pieces 
-    // are unchanged
-    auto rotate_face = [&](Face face){
-        // define even and odd subgroups
-        int even[] = {0, 2, 8, 6};
-        int odd[] = {1, 5, 7, 3};
-
-        // 'invert' the subgroups
-        if (move == Fi){
-            std::swap(even[1], even[3]);
-            std::swap(odd[1], odd[3]);
-        }
-
-        // assign the colors
-        for (int i = 0; i+1 < 4; ++i){
-            new_rubik.cube[face][even[i+1]] = cube[face][even[i]];
-            new_rubik.cube[face][odd[i+1]] = cube[face][odd[i]];
-        }
-        new_rubik.cube[face][even[0]] = cube[face][even[3]];
-        new_rubik.cube[face][odd[0]] = cube[face][odd[3]];
-    };
 
     // Define a lambda to rotate the edge pieces.
     // f1 is the dest face and f2 is the source face.
@@ -157,46 +155,131 @@ Rubik3 Rubik3::next(Move move){
             new_rubik.cube[f1][f1Idx[1]] = cube[f2][f2Idx[1]];
             new_rubik.cube[f1][f1Idx[2]] = cube[f2][f2Idx[2]];
         };
+
+    // Nested block to perform face rotation
+    {
+        // define even and odd subgroups
+        int even[] = {0, 2, 8, 6};
+        int odd[] = {1, 5, 7, 3};
+
+        // 'invert' the subgroups
+        if (move == Fi or move == Ri or move == Li or 
+            move == Bi or move == Ui){
+            std::swap(even[1], even[3]);
+            std::swap(odd[1], odd[3]);
+        }
+
+        // determine face to rotate
+        Face face;
+        switch(move){
+        case R:case Ri:    face = RIGHT;   break;
+        case L:case Li:    face = LEFT;    break;
+        case B:case Bi:    face = BACK;    break;
+        case D:case Di:    face = DOWN;    break;
+        case F:case Fi:    face = FRONT;   break;
+        case U:case Ui:    face = UP;      break;
+        }
+
+        // assign the colors
+        for (int i = 0; i+1 < 4; ++i){
+            new_rubik.cube[face][even[i+1]] = cube[face][even[i]];
+            new_rubik.cube[face][odd[i+1]] = cube[face][odd[i]];
+        }
+        new_rubik.cube[face][even[0]] = cube[face][even[3]];
+        new_rubik.cube[face][odd[0]] = cube[face][odd[3]];
+    }
     
     if (move == Move::F){
-        rotate_face(FRONT);
         rotate_edge_pieces(RIGHT, {0,3,6}, UP, {6,7,8});
         rotate_edge_pieces(DOWN, {0,1,2}, RIGHT, {0,3,6});
         rotate_edge_pieces(LEFT, {2,5,8}, DOWN, {0,1,2});
         rotate_edge_pieces(UP, {6,7,8}, LEFT, {2,5,8});
     }
     else if (move == Fi){
-        rotate_face(FRONT);
         rotate_edge_pieces(LEFT, {2,5,8}, UP, {6,7,8});
         rotate_edge_pieces(UP, {6,7,8}, RIGHT, {0,3,6});
         rotate_edge_pieces(RIGHT, {0,3,6}, DOWN, {0,1,2});
         rotate_edge_pieces(DOWN, {0,1,2}, LEFT, {2,5,8});
     }
     else if (move == B){
-        rotate_face(BACK);
         rotate_edge_pieces(LEFT, {0,3,6}, UP, {0,1,2});
         rotate_edge_pieces(UP, {0,1,2}, RIGHT, {2,5,8});
         rotate_edge_pieces(RIGHT, {2,5,8}, DOWN, {6,7,8});
         rotate_edge_pieces(DOWN, {6,7,8}, LEFT, {0,3,6});
     }
     else if (move == Bi){
-        rotate_face(BACK);
         rotate_edge_pieces(RIGHT, {2,5,8}, UP, {0,1,2});
         rotate_edge_pieces(LEFT, {0,3,6}, DOWN, {6,7,8});
         rotate_edge_pieces(UP, {0,1,2}, LEFT, {0,3,6});
-        rotate_edge_pieces(DOWN, {6,7,8}, RIGHT, {2,5,8});
+        rotate_edge_pieces(DOWN, {8,7,6}, RIGHT, {2,5,8});
     }
+    else if (move == L){
+        rotate_edge_pieces(FRONT, {0,3,6}, UP, {0,3,6});
+        rotate_edge_pieces(DOWN, {0,3,6}, FRONT, {0,3,6});
+        rotate_edge_pieces(BACK, {2,5,8}, DOWN, {0,3,6});
+        rotate_edge_pieces(UP, {0,3,6}, BACK, {8,5,2});
+    }
+    else if (move == Li){
+        rotate_edge_pieces(BACK, {8,5,2}, UP, {0,3,6});
+        rotate_edge_pieces(DOWN, {0,3,6}, BACK, {2,5,8});
+        rotate_edge_pieces(FRONT, {0,3,6}, DOWN, {0,3,6});
+        rotate_edge_pieces(UP, {0,3,6}, FRONT, {0,3,6});
+    }
+    else if (move == R){
+        rotate_edge_pieces(BACK, {0,3,6}, UP, {2,5,8});
+        rotate_edge_pieces(DOWN, {2,5,8}, BACK, {0,3,6});
+        rotate_edge_pieces(FRONT, {2,5,8}, DOWN, {2,5,8});
+        rotate_edge_pieces(UP, {2,5,8}, FRONT, {2,5,8});
+    }
+    else if (move == Ri){
+        rotate_edge_pieces(FRONT, {2,5,8}, UP, {2,5,8});
+        rotate_edge_pieces(DOWN, {2,5,8}, FRONT, {2,5,8});
+        rotate_edge_pieces(BACK, {6,3,0}, DOWN, {2,5,8});
+        rotate_edge_pieces(UP, {2,5,8}, BACK, {0,3,6});
+    }
+    else if (move == U){
+        rotate_edge_pieces(LEFT, {0,1,2}, FRONT, {0,1,2});
+        rotate_edge_pieces(BACK, {0,1,2}, LEFT, {0,1,2});
+        rotate_edge_pieces(RIGHT, {0,1,2}, BACK, {0,1,2});
+        rotate_edge_pieces(FRONT, {0,1,2}, RIGHT, {0,1,2});
+    }
+    else if (move == Ui){
+        rotate_edge_pieces(FRONT, {0,1,2}, LEFT, {0,1,2});
+        rotate_edge_pieces(LEFT, {0,1,2}, BACK, {0,1,2});
+        rotate_edge_pieces(BACK, {0,1,2}, RIGHT, {0,1,2});
+        rotate_edge_pieces(RIGHT, {0,1,2}, FRONT, {0,1,2});
+    }
+    #ifdef DEBUG
+    new_rubik.unfold_and_show();
+    #endif
     return new_rubik;
+}
+
+bool Rubik3::is_identity(){
+    auto all_same = [this](Face f) -> bool {
+        for (int i = 0; i+1 < N*N; ++i)
+            if (cube[f][i+1] != cube[f][i])
+                return false;
+        return true;
+    };
+
+    for (const auto& [face, v] : cube)
+        if (!all_same(face))
+            return false;
+    return true;
 }
 
 int main(){
     Rubik3 cube;
     // cube.show();
-    cube.unfold_and_show();
-    cube.next(F).next(Fi).unfold_and_show();
-    cube.next(B).unfold_and_show();
-    cube.next(Bi).unfold_and_show();
-    cube.next(B).next(Bi).unfold_and_show();
-   
+    // cube.unfold_and_show();
+    // cube.next(F).next(Fi).unfold_and_show();
+    // cube.next(B).unfold_and_show();
+    // cube.next(Bi).unfold_and_show();
+    // cube.next(B).next(Bi).unfold_and_show();
+    // cube/*.next(L)*/.next(Li).unfold_and_show();
+    // cube.next(Ri).next(R).unfold_and_show();
+    // std::cout << cube.next(R).next(Ri).is_identity() << std::endl;
+    cube.next(Fi).next(Li).next(L).next(Ri).next(Bi).next(U).next(Ui);
     return 0;
 }
